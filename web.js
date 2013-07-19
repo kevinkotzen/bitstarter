7590592d03e1efafd2a190694677f6524fd6fdbd
Kevin Kotzen <kevinkotzen@gmail.com>
2013-07-19 09:54:31 +0000

    Reset

--- web.js
+++ web.js
@@ -2,14 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var fs = require('fs');
-//var filename = "index.html";
-//var fileread = fs.readFileSync(filename).toString();
-//var fileread = "Hello World !!!";
-
-
 app.get('/', function(request, response) {
-  response.send("Hello World !!!!!"));
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
