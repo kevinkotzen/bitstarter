a5d7e2cbefed90b3ea06a2eabe1592875b0bb2c3
Kevin Kotzen <kevinkotzen@gmail.com>
2013-07-19 09:51:27 +0000

    Still debugging

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var app = express.createServer(express.logger());
 //var fs = require('fs');
 //var filename = "index.html";
 //var fileread = fs.readFileSync(filename).toString();
-var fileread = "Hello World !!!";
+//var fileread = "Hello World !!!";
 
 
 app.get('/', function(request, response) {
-  response.send(fileread));
+  response.send("Hello World !!!!!"));
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
